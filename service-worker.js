5de7a6c2384c99037409d52d24282eae9de9bd2a
DronVol <dronvol@gmail.com>
2019-01-14 17:43:23 +0300

    indented print added

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","43299c1bbedc0fd14ee67642947ee1cf"],["static/css/main.8614fb72.css","5a0160a3a1ea2da9ca4e0de289dbeebd"],["static/js/main.60dddfe2.js","dc5d6cd424a55a5b52660a0917cd32c2"],["static/media/icon.11ca7b92.png","11ca7b92370f18380a43de22e3f32861"],["static/media/sync.9e0b360e.svg","9e0b360e83dd815422241d74c1674d02"]];
+var precacheConfig = [["index.html","ad8206513cb625982c420018002464d1"],["static/css/main.8614fb72.css","5a0160a3a1ea2da9ca4e0de289dbeebd"],["static/js/main.093428af.js","f39726b34f76b10cd0a9877efe6b561c"],["static/media/icon.11ca7b92.png","11ca7b92370f18380a43de22e3f32861"],["static/media/sync.9e0b360e.svg","9e0b360e83dd815422241d74c1674d02"]];
 var cacheName = 'sw-precache-v3-sw-precache-webpack-plugin-' + (self.registration ? self.registration.scope : '');
 
 
